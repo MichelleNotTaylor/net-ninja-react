9657cbce47152bfb4316787a5ef736cc7b5b1b17
CodeMichelle <michelle.taylor@parchment.com>
2022-03-08 21:54:44 -0600

    Error handling

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,39 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
+  const [blogs, setBlogs] = useState (null);
+  const [isLoading, setIsLoading] = useState (true);
+  const [error, setError] = useState (null);
 
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => { 
-            return res.json(); 
+  useEffect (() => {
+    setTimeout (() => {
+      fetch ('http://localhost:8001/blogs')
+        .then (res => {
+          if (!res.ok) {
+            throw Error ('Could not fetch data for that result.');
+          }
+          return res.json ();
         })
-        .then(data => {
-            setBlogs(data);
+        .then (data => {
+          setBlogs (data);
+          setIsLoading (false);
+          setError (null);
         })
-    }, []);
+        .catch (err => {
+          setIsLoading (false);
+          setError (err.message);
+        });
+    }, 1000);
+  }, []);
 
-    return (
-        <div className="home">
-            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {isLoading && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+    </div>
+  );
+};
 
 export default Home;
