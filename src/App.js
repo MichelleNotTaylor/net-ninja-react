9a54e959fcd9618c41af4cc9d9a0d40754def8c3
CodeMichelle <michelle.taylor@parchment.com>
2022-03-09 22:23:10 -0600

    Finished dojo blog. Added final 404 page for unknown paths.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App () {
   const title = 'App Component';
@@ -21,6 +22,9 @@ function App () {
             </Route>
             <Route exact path={'/blogs/:id'}>
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
