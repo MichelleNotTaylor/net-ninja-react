7390ea527ae5a1a68543d0d1619caabf7c2edb76
CodeMichelle <michelle.taylor@parchment.com>
2022-03-09 20:46:35 -0600

    Added abort controller to data fetch.

--- src/useFetch.js
+++ src/useFetch.js
@@ -5,26 +5,36 @@ const useFetch = url => {
   const [isLoading, setIsLoading] = useState (true);
   const [error, setError] = useState (null);
 
-  useEffect (() => {
-    setTimeout (() => {
-      fetch (url)
-        .then (res => {
-          if (!res.ok) {
-            throw Error ('Could not fetch data for that result.');
-          }
-          return res.json ();
-        })
-        .then (data => {
-          setData (data);
-          setIsLoading (false);
-          setError (null);
-        })
-        .catch (err => {
-          setIsLoading (false);
-          setError (err.message);
-        });
-    }, 1000);
-  }, []);
+  useEffect (
+    () => {
+      const abortCont = new AbortController ();
+
+      setTimeout (() => {
+        fetch (url, {signal: abortCont.signal})
+          .then (res => {
+            if (!res.ok) {
+              throw Error ('Could not fetch data for that result.');
+            }
+            return res.json ();
+          })
+          .then (data => {
+            setData (data);
+            setIsLoading (false);
+            setError (null);
+          })
+          .catch (err => {
+            if (err.name === 'AbortError') {
+              console.log ('Fetch aborted.');
+            } else {
+              setIsLoading (false);
+              setError (err.message);
+            }
+          });
+      }, 1000);
+      return () => abortCont.abort ();
+    },
+    [url]
+  );
 
   return {data, isLoading, error};
 };
