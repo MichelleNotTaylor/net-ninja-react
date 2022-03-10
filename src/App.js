d4bd93653a700a19a7213ab5b6f636f350581329
CodeMichelle <michelle.taylor@parchment.com>
2022-03-09 20:33:40 -0600

    Create path

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Create from './Create';
 
-function App() {
+function App () {
   const title = 'App Component';
 
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="content">
-        <Home/>
-      </div>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path={'/'}>
+              <Home />
+            </Route>
+            <Route exact path={'/create'}>
+              <Create />
+            </Route>
+          </Switch>
+        </div>
+      </div>;
+
+    </Router>
   );
 }
 
